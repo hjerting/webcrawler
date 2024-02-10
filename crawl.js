49a353bce8210b5f3cf2426724041a472b123517
Jon Hjerting <jon@hjerting.com>
2024-02-10 07:04:22 +0100

    Added some tests for normalizeURL function.

--- crawl.js
+++ crawl.js
@@ -1,7 +1,13 @@
 function normalizeURL(urlString) {
-    return urlString;
+    const urlObject = new URL(urlString);
+    const hostPath = `${urlObject.hostname}${urlObject.pathname}`;
+    const last = hostPath.length - 1;
+    if (hostPath.length > 0 && hostPath[last] === '/') {
+        return hostPath.slice(0, last);
+    }
+    return hostPath;
 }
 
 module.exports = {
     normalizeURL
-}
+};
